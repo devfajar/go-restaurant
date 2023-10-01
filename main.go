3fd75b47452a6f82df16b40c31f4a8ad87e22c74
Devfajar <fajartrengginas@gmail.com>
2023-10-01 20:21:09 +0700

    Adding: Base Structure Project

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/devfajar/go-restaurant/database"
+	"github.com/devfajar/go-restaurant/middleware"
+	"github.com/devfajar/go-restaurant/routes"
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.User(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
